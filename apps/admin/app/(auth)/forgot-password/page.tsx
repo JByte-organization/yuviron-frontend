4bcffbb50db1b27237a62e9bb11d5d5f6ae5b9be
Николай Филиппов <filippovnikolay195@gmail.com>
2026-03-05 12:56:11 +0200

    fix empty page for build project

---
+++ apps/admin/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+export default function ForgotPasswordPage() {
+    return (
+        <main>
+            <h1>Forgot password</h1>
+        </main>
+    )
+}
---
+++ apps/admin/app/(auth)/repeat-password/page.tsx
@@ -0,0 +1,7 @@
+export default function RepeatPasswordPage() {
+    return (
+        <main>
+            <h1>Repeat Password</h1>
+        </main>
+    )
+}
