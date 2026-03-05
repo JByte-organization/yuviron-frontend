62e10ebfa4e14e34858ca804a9ecd3385ce64bc7
Mariasshev <sevcenkomaria72@gmail.com>
2026-03-05 20:41:37 +0200

    change structure

--- apps/admin/app/(dashboard)/layout.tsx
+++ apps/admin/app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import { Sidebar } from '@/components/layout/Sidebar';
 import "@repo/ui/src/styles/main.scss";
 
 
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="admin-layout">
