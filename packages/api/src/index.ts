e924997b9de4a381bd018f1f22b02d34b83882f9
Mariasshev <sevcenkomaria72@gmail.com>
2026-05-28 14:44:28 +0300

    fix Login, change Favourite Page

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -37,6 +37,7 @@ export * from './generated/client/models/followedArtistDto';
 export * from './generated/client/models/userPlaylistDto';
 export * from './generated/client/models/recentlyPlayedTrackDto';
 export * from './generated/client/models/currentUserDto';
+export * from './generated/client/models/userFavoriteTrackDto';
 
 
 export { configureApiClient } from './mutator';
