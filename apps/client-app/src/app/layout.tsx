72334547f2f4a9f02275a1920fbb87b7ac7befef
Mariasshev <sevcenkomaria72@gmail.com>
2026-06-01 22:38:09 +0300

    add favicon

--- apps/client-app/src/app/layout.tsx
+++ apps/client-app/src/app/layout.tsx
@@ -7,6 +7,26 @@ import "@repo/ui/styles";
 
 export const metadata: Metadata = {
     title: "Yuviron - Музика для кожного",
+    description: "Слухай улюблену музику на Yuviron",
+
+    manifest: "/images/favicon/site.webmanifest",
+
+    appleWebApp: {
+        title: "Yuviron",
+        statusBarStyle: "default",
+        capable: true,
+    },
+
+    icons: {
+        icon: [
+            { url: "/images/favicon/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+            { url: "/images/favicon/favicon.svg", type: "image/svg+xml" },
+        ],
+        shortcut: "/images/favicon/favicon.ico",
+        apple: [
+            { url: "/images/favicon/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+        ],
+    },
 };
 
 export default function RootLayout({
