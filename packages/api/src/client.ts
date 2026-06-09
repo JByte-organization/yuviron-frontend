34699c3189a01927cd986e35a5b1b9682fb8f901
Mariasshev <sevcenkomaria72@gmail.com>
2026-06-09 22:56:36 +0300

    fix client

--- packages/api/src/client.ts
+++ packages/api/src/client.ts
@@ -14,6 +14,7 @@ export * from './generated/client/endpoints/playlists';
 export * from './generated/client/endpoints/notifications';
 export * from './generated/client/endpoints/users';
 export * from './generated/client/endpoints/albums';
+export * from './generated/client/endpoints/account';
 export { postApiAnalyticsPlayStart, postApiAnalyticsPlayCommit } from './generated/client/endpoints/analytics';
 
 // МОДЕЛІ КЛІЄНТА — тепер імпортуємо ВСІ однією строкою без конфліктів!
